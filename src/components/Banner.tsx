82cafadc397dde7a3d4aee3107837cf563f16171
Andrea Iriarte <andreairiarte@Andreas-Mac-mini.local>
2023-07-25 16:51:04 -0400

    build errors

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const Banner = () => {
   return (
     <div className='desktop:w-[40%] w-full desktop:h-full h-[40%] bg-cover  bg-hero desktop:rounded-l-[15px] rounded-t-[15px] desktop:rounded-tr-[0px] bg-no-repeat' />
