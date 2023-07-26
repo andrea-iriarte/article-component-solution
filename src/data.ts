938b4c072adc1dfc1f7e467937a3b6b2c49707d6
Andrea Iriarte <andreairiarte@Andreas-Mac-mini.local>
2023-07-25 21:59:44 -0400

    altered profile pic url path

--- src/data.ts
+++ src/data.ts
@@ -10,6 +10,6 @@ export const article: Article  = {
     headline: "Shift the overall look and feel by adding these wonderful touches to furniture in your home",
     bodyPreview: "Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete.",
     author: "Michelle Appleton",
-    profilePic: "avatar-michelle.jpg",
+    profilePic: "/assets/avatar-michelle.jpg",
     date: "28 Jun 2020"
 }
