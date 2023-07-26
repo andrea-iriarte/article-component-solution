28e8afc97a5c773a26ee28434f1df53dcbd26d86
Andrea Iriarte <andreairiarte@Andreas-Mac-mini.local>
2023-07-25 22:10:25 -0400

    commenting out unnecessary code

--- src/data.ts
+++ src/data.ts
@@ -3,13 +3,13 @@ export type Article = {
     headline: string,
     bodyPreview: string, 
     author: string,
-    profilePic: string,
+    /* profilePic: string, */
     date: string
 }
 export const article: Article  = {
     headline: "Shift the overall look and feel by adding these wonderful touches to furniture in your home",
     bodyPreview: "Ever been in a room and felt like something was missing? Perhaps it felt slightly bare and uninviting. I've got some simple tips to help you make any room feel complete.",
     author: "Michelle Appleton",
-    profilePic: "../assets/avatar-michelle.jpg",
+    /* profilePic: "../assets/avatar-michelle.jpg", */
     date: "28 Jun 2020"
 }
